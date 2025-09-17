be5aba3f625a52634f23dfab0cdd5d1b3baafc9d
Nisrina <nisrina.fatimah@ui.ac.id>
2025-09-17 10:19:58 +0700

    fix pws

--- main/models.py
+++ main/models.py
@@ -22,7 +22,7 @@ SLEEVE_CHOICES = [
 ]
 
 class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100) 
     price = models.IntegerField()                          
     description = models.TextField()                       
