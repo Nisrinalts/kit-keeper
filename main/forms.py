5dcbdbfeb7e02903da1421073626e20e7bcb24b8
Nisrina <nisrina.fatimah@ui.ac.id>
2025-09-17 10:10:45 +0700

    fix pws

--- main/forms.py
+++ main/forms.py
@@ -7,7 +7,4 @@ class ProductForm(ModelForm):
         fields = [
             "name", "price", "description", "thumbnail", "category", "is_featured",
             "team", "season", "size", "sleeve_type", "condition", "manufacturer", "stock",
-        ]
-        labels = {
-            "sleeve_type": "Sleeve",
-        }
+        ]   
